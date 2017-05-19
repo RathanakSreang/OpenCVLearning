680fa59ee0f2960b0eaaf239d63a6381eebcc969
Rathanak Sreang <sreang.rathanak@framgia.com>
2017-05-19 08:04:40 +0700

    Recommendation

---
+++ apply_deep_learning/rbm.py
@@ -0,0 +1,12 @@
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.neurallnetwork import BernoulliRBM
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipleline
+import numpy as np
+import cv2
+import argparse
+import time
+
+
